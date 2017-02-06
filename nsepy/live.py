8c335bbcc370a7163448e0e35299d116ad705a9a
Dhruv Mishra <ubiquitiousdm@gmail.com>
2017-02-06 06:38:32 +0530

    added function for option chain

--- nsepy/live.py
+++ nsepy/live.py
@@ -7,6 +7,7 @@ Created on Fri Dec 18 21:51:41 2015
 from nsepy.commons import *
 import ast
 import json
+from bs4 import BeautifulSoup
 from nsepy.liveurls import quote_eq_url, quote_derivative_url, option_chain_url
 
 
@@ -19,11 +20,41 @@ def get_quote(symbol, series='EQ', instrument=None, expiry=None, option_type=Non
     4. type (CE/PE for options, - for futures
     5. strike (strike price upto two decimal places
     """
-
-    if instrument:
-        expiry_str = "%02d%s%d"%(expiry.day, months[expiry.month][0:3].upper(), expiry.year)
+    expiry_str = "%02d%s%d"%(expiry.day, months[expiry.month][0:3].upper(), expiry.year)
+    if instrument == 'FUTSTK' or instrument == 'FUTIDX':
         res = quote_derivative_url(symbol, instrument, expiry_str, option_type, strike)
-
-    res = quote_eq_url(symbol, series)
+    else:
+        res = quote_eq_url(symbol, series)
 
     return json.loads(res.text)['data'][0]
+
+
+def get_option_chain(symbol, series='EQ', instrument=None, expiry=None):
+    """
+    1. Underlying security (stock symbol or index name)
+    2. instrument (OPTSTK, OPTIDX)
+    3. expiry (ddMMMyyyy)
+    """
+    OPTION_SCHEMA = [str, int, int, int, float, float, float,
+                    int, float, float, int, float,
+                    int, float, float, int, float,
+                    float, float, int, int, int, str]
+    OPTION_HEADERS = ['Chart', 'OI CE', 'Chng in OI CE', 'Volume CE', 'IV CE',
+                  'LTP CE', 'Net Chng CE', 'Bid Qty CE', 'Bid Price CE',
+                  'Ask Price CE', 'Ask Qty CE', 'Strike Price', 'Bid Qty PE', 'Bid Price PE',
+                  'Ask Price PE', 'Ask Qty PE', 'Net Chng PE', 'LTP PE', 'IV PE',
+                  'Volume PE', 'Chng in OI PE', 'OI PE', 'Chart PE']
+    expiry_str = "%02d%s%d"%(expiry.day, months[expiry.month][0:3].upper(), expiry.year)
+    if instrument == 'OPTSTK' or instrument == 'OPTIDX':
+        res = option_chain_url(symbol, instrument, expiry_str)
+        bs = BeautifulSoup(res.text, 'html.parser')
+        tp = ParseTables(soup=bs,
+                     schema=OPTION_SCHEMA,
+                     headers=OPTION_HEADERS, index="Strike Price")
+        df = tp.get_df()
+        return df.drop(['Chart', 'Chart PE'], axis=1)
+
+
+# q = get_quote(symbol='NIFTY', instrument='FUTIDX', expiry=datetime.date(2017,02,23))
+# q = get_option_chain(symbol='SBIN', instrument='OPTSTK', expiry=datetime.date(2017,02,23))
+# print(q)
